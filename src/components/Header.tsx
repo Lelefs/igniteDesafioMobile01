bc8b10d170a5dc22bc16a4b1b657ee3ce653e15e
Leandro Finochio <lefinochio@hotmail.com>
2021-05-18 15:10:15 -0300

    First commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View, Text, StatusBar, StyleSheet, TouchableOpacity } from 'react-native';
+
+interface HeaderProps {
+  theme: string;
+  onUserAction: () => void;
+}
+
+export function Header({ theme, onUserAction }: HeaderProps) {
+  return (
+    <View style={[styles.header]}>
+      <Text style={styles.headerText}>to.</Text>
+      <Text style={[styles.headerText, { fontFamily: 'Poppins-SemiBold' }]}>do</Text>
+      <TouchableOpacity style={styles.headerButton} activeOpacity={0.7} onPress={onUserAction}>
+        <Text style={styles.headerButtonText}>
+          {theme === 'light' ? 'DARK' : 'LIGHT'}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  header: {
+    paddingTop: StatusBar.currentHeight,
+    paddingBottom: 44,
+    backgroundColor: '#273FAD',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  headerText: {
+    fontSize: 24,
+    color: '#FFF',
+    fontFamily: 'Poppins-Regular',
+  },
+  headerButton: {
+    position: 'absolute',
+    right: 20,
+    borderColor: '#fff',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5
+  },
+  headerButtonText: {
+    color: '#fff',
+    fontWeight: 'bold'
+  }
+});
---
+++ src/pages/Home.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+
+import { Header } from '../components/Header';
+import { MyTasksList } from '../components/MyTasksList';
+import { TodoInput } from '../components/TodoInput';
+
+interface Task {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+export function Home() {
+  const [theme, setTheme] = useState('light');
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  function handleChangeTheme() {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  }
+
+  function handleAddTask(newTaskTitle: string) {
+    if (!newTaskTitle) return null;
+
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    setTasks(oldState => [ ...oldState, newTask ]);
+  }
+
+  function handleMarkTaskAsDone(id: number) {
+    const newTasks = tasks.map(task => {
+      if (task.id === id) {
+        task.done = !task.done
+      }
+
+      return task
+    })
+
+    setTasks(newTasks);
+  }
+
+  function handleRemoveTask(id: number) {
+    setTasks(oldState => oldState.filter(
+      task => task.id !== id
+    ))
+  }
+
+  return (
+    <>
+      <Header theme={theme} onUserAction={handleChangeTheme} />
+
+      <TodoInput theme={theme} addTask={handleAddTask} />
+
+      <MyTasksList
+        theme={theme}
+        tasks={tasks} 
+        onPress={handleMarkTaskAsDone} 
+        onLongPress={handleRemoveTask} 
+      />
+    </>
+  )
+}
